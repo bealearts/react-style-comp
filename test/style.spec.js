144cfc886fdc4169628cd73923c970492241f2b9
davidbeale <david.beale@bealearts.co.uk>
2017-06-23 08:55:36 +0100

    Init test setup

---
+++ test/style.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import { mount } from 'enzyme';
+
+describe('react-style-comp', () => {
+    it('should apply local CSS rules', () => {
+    
+    });
+}); 
